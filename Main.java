23ca8ac6e6c092b69e0e02678eba26b4b26e567a
cameleon <45.worker@protonmail.com>
2021-11-26 16:04:40 +0100

    Amélioration non vérifier

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package Projetpoo;
 
 import java.time.LocalDate;
 
-public class Main {
+public class Main { 
 
     private class Etudiant {
         int numero_etu;
@@ -10,25 +10,45 @@ public class Main {
         String prenom;
         String telephone;
         String mail;
-        byte annee_formation;
-        char q; //étant compris entre 1 et 5 pour limiter la ram usage
+        byte annee_formation; //étant compris entre 1 et 5 pour limiter la ram usage
     }
 
-    private class Personel {
+    private class Personnel {
         int numero;
         String nom;
         String prenom;
         String telephone;
         String mail;
+        
+        int max-table = 10;  // 1 table = 8 places
+        int max-place = 80;
+        
+        Boolean validé = False;
+        string est-accompagné;
+        
+        while (validé == False){
+        Scanner accompagné = new Scanner(System.in);
+        est-accompagné = accompagné.next();
+        
+          if (est-accompagné.equals("oui") || est-accompagné.equals("OUI") || est-accompagné.equals("Oui") ){
+              est-accompagné = oui; 
+              validé = True;
+          }else if(est-accompagné.equals("non") || est-accompagné.equals("NON") || est-accompagné.equals("Non")){
+              est-accompagné = non; 
+              validé = True;
+          }
+        Systemout.println(nom + prenom + "+" + est-accompagné)
+        
+        
     }
 
     private class Table {
-        int num_table;
-        int place_libre; //8 place libre
+        int numero_table;
+        int place_libre; //8 places libres
 
         public void calcul_place() {
             if (place_libre == 0) {
-                num_table += 1;
+                numumero_table += 1;
                 place_libre = 8;
             } else {
                 place_libre -= 1;
@@ -44,4 +64,4 @@ public class Main {
         }
 
     }
-}
+}    
