cc7c1a136499f518e91e5bfc2ceb6bc33f784357
cameleon <86978737+Cameleon00722@users.noreply.github.com>
2021-12-02 09:14:43 +0100

    Update Controleur.java
    Add controleur

--- Controleur.java
+++ Controleur.java
@@ -1,1 +1,30 @@
+package projet;
 
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+public class Controleur {
+    Gala monGala;
+
+    public Controleur(LocalDate date) throws IOException, ClassNotFoundException {
+        ServiceStockage entrepot= new ServiceStockage();
+        if(entrepot.charger()==null){
+            monGala=new Gala(date);
+        }else{
+            monGala=(Gala) entrepot.charger();
+        }
+
+
+    }
+    public void ctlAjouterTable(Object table){
+
+
+    }
+    public void quitter() throws IOException {
+        ServiceStockage entrepot= new ServiceStockage();
+        entrepot.enregistrer(monGala);
+    }
+}
