67923bc0ba6a6959aeb7c2a4c68541a9e1921193
cameleon <86978737+Cameleon00722@users.noreply.github.com>
2021-12-08 17:34:59 +0100

    Update Ihm.java

--- Ihm.java
+++ Ihm.java
@@ -1,4 +1,53 @@
-package projet;
+package Projetpoo;
 
-public class Ihm {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+//inscription et désincription
+public class Ihm implements ActionListener
+{
+    JFrame frame = new JFrame("Gestion Gala");
+
+    JLabel label = new JLabel("Choisissez une option", JLabel.CENTER);
+
+
+    // Définir les boutons
+    JButton btn1 = new JButton("inscription");
+    JButton btn2 = new JButton("désincription");
+
+    public Ihm() {
+
+        // Définissez le frame
+
+        frame.add(label);
+
+        // Définissez le panel
+        JPanel panel = new JPanel();
+
+        // Ajouter les boutons au frame
+        panel.add(btn1);
+        panel.add(btn2);
+
+        // Ajouter label et panel au frame
+
+        frame.add(label);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 350);
+        frame.setLayout(new GridLayout(2, 1));
+        frame.setVisible(true);
+        btn1.addActionListener(this);
+
+        frame.pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==btn1){
+            frame.setVisible(false);
+            choixtype fen3 = new choixtype();
+        }
+    }
 }
